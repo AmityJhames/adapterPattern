baf1405c8aa068ed9215dd5cb52048d8f7708e8b
Amity Jhames Abaja <143429028+AmityJhames@users.noreply.github.com>
2023-10-24 22:41:05 +0800

    Update Refrigirator.java

--- Refrigirator.java
+++ Refrigirator.java
@@ -1,13 +1,12 @@
 package adapterPattern;
 
 public class Refrigirator {
-        public String startCooling(){
-            return " *beep*...Initializing cooling stage!";
+   public String startCooling(){
+            return "*beep*...Initializing cooling stage!";
         }
 
         public String turnOff(){
             return "Your Fridge is shutting down....";
         }
-
 }
 
